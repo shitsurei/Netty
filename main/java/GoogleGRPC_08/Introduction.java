45bccbd23fe36f503eb8ea12c967f42c8295aeb7
shitsurei <lfgewj21345@163.com>
2020-03-27 23:58:07 +0800

    gradlew命令详解

--- main/java/GoogleGRPC_08/Introduction.java
+++ main/java/GoogleGRPC_08/Introduction.java
@@ -1,13 +1,16 @@
 package GoogleGRPC_08;
 
-import java.util.*;
-
 /**
  * 01
  * grpc：高性能，开源，统一的rpc框架，可以使用Protobuf3定义服务
  * 自动生成符合约定的客户端和服务端stub
  * 实现客户端和服务端的双向流式通信，基于HTTP2标准
  * 支持客户端和服务端之间通过流来传递一系列对象，类似WebSocket长连接
+ *
+ * gradlew => gradle wrapper 包装器
+ * 开发者在本地没有安装gradle的前提下也可以使用gradlew命令实现对项目的构建
+ * 使用该命令同样可以对本地项目的gradle环境进行打包，生成相应的配置文件（gradle-wrapper.properties）、jar包和针对Windows系统和iOS系统的批处理文件
+ * gradlew命令检测本地是否安装了对应版本的gradle时默认会从用户目录下的.gradle/wrapper/dists目录中查询（可以在配置文件中修改）
  */
 public class Introduction {
     public static void main(String[] args) {
